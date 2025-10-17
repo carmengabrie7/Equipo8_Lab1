1b55bed0271a58e84b42a43493af6baf7b682adc
Carmen E <carmenelisa.gabrie7@gmail.com>
2025-10-17 10:53:43 -0600

    Funciones

--- src/equipo8_lab1/Funciones_Recursivas.java
+++ src/equipo8_lab1/Funciones_Recursivas.java
@@ -5,14 +5,14 @@ public class Funciones_Recursivas {
  public static int contarNOLEIDOS(Email[] inbox, int pos){
      if (pos==inbox.length)//caso base
          return 0; 
-     if (inbox[pos] != null && !inbox[pos].isLeido())
+     if (inbox[pos] != null && !inbox[pos].isLeido())//revisa si el correo existe y si esta leido
         return  contarNOLEIDOS(inbox, pos +1) + 1; 
      //tipo de método - down
      return contarNOLEIDOS(inbox,pos+1);
  }
 
  public static void buscaPOREMISOR (Email[] inbox,int pos, String emisorBuscado){
-     if (pos==inbox.length)
+     if (pos==inbox.length)//caso base
      return;
      if (inbox[pos]!= null && inbox[pos].Emisor.equalsIgnoreCase(emisorBuscado))
          System.out.println(inbox[pos]);
@@ -20,20 +20,23 @@ public class Funciones_Recursivas {
  }
  
  public static void mostrarTODO (Email[] inbox,int pos){
-     if (pos==inbox.length)
+     if (pos==inbox.length)//caso base
      return;
-     if (inbox[pos]!=null)
+     if (inbox[pos]!=null) 
          System.out.println(inbox[pos]);
      mostrarTODO(inbox, pos +1);
+ }
  
- 
- public static void eliminarLEIDOS(){
-     
+ public static void eliminarCORREOS (Email[] inbox,int pos){
+     if (pos == inbox.length)//caso base
+     return;
+     if (inbox[pos]!=null && inbox[pos].isLeido())
+         inbox[pos]=null; //borra los correos
      return;
  }
  
  public static int contarTOTAL (Email[]inbox,int pos){
-     if (pos==inbox.length)
+     if (pos==inbox.length)//caso base
          return 0;
      if (inbox[pos] != null)
          return contarTOTAL (inbox, pos +1)+1;
