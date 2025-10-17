891089dc69706eaac52a375a90bd772516935005
Carmen E <carmenelisa.gabrie7@gmail.com>
2025-10-17 10:29:52 -0600

    Progreso en funciones

--- src/equipo8_lab1/Funciones_Recursivas.java
+++ src/equipo8_lab1/Funciones_Recursivas.java
@@ -5,29 +5,34 @@ public class Funciones_Recursivas {
  public static int contarNOLEIDOS(Email[] inbox, int pos){
      if (pos==inbox.length)//caso base
          return 0; 
-     if (inbox[pos] != null && !inbox[pos].leido)
-        return  1 + contarNOLEIDOS(inbox, pos +1); 
-     //tipo de método - downk
+     if (inbox[pos] != null && !inbox[pos].isLeido())
+        return  contarNOLEIDOS(inbox, pos +1) + 1; 
+     //tipo de método - down
  }
 
- public static int buscaPOREMISOR (Email[] inbox,int pos, String emisorBuscado){
+ public static void buscaPOREMISOR (Email[] inbox,int pos, String emisorBuscado){
+     
+     
+     return;
+ }
+ 
+ public static void mostrarTODO (){
+     
+     return;
+ }
+ 
+ public static void eliminarLEIDOS(){
+     
+     return;
+ }
+ 
+ public static int contarTOTAL (Email[]inbox,int pos){
      if (pos==inbox.length)
          return 0;
-     if (inbox[pos] != null )
-     
-     
-     return 0;
- }
- 
- public static int mostrarTODO (){
-     
-     return 0;
- }
- 
- public static int contarTOTAL (Email[]inbox){
-     
+     if (inbox[pos] != null)
+         return contarTOTAL (inbox, pos +1)+1;
          
-     return 0;
+     return contarTOTAL (inbox,pos +1);
  }
     
 }
