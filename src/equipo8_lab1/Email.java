6385d90c7e59b1b468b9093e7766589a18f74738
tusderechos <hidrosoluciones61@gmail.com>
2025-10-17 09:00:55 -0600

    Completacion de la clase Email con sus getters y la funcion 'print', con su formato de impresion de correos.

--- src/equipo8_lab1/Email.java
+++ src/equipo8_lab1/Email.java
@@ -4,10 +4,55 @@
  */
 package equipo8_lab1;
 
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
 /**
  *
  * @author Hp
  */
 public class Email {
+    
+    public static String Emisor;
+    public static String Asunto;
+    public static String Contenido;
+    public static boolean Leido;
+    public Calendar FechaEnvio;
+
+    public Email(String Emisor, String Asunto, String Contenido) {
+        this.Emisor = Emisor;
+        this.Asunto = Asunto;
+        this.Contenido = Contenido;
+        Leido = false;
+        FechaEnvio = Calendar.getInstance();
+    }
+
+    public String getEmisor() {
+        return Emisor;
+    }
+
+    public String getAsunto() {
+        return Asunto;
+    }
+
+    public String getContenido() {
+        return Contenido;
+    }
+
+    public boolean isLeido() {
+        return Leido;
+    }
+
+    public Calendar getFechaEnvio() {
+        return FechaEnvio;
+    }
+    
+    public String Print() {
+        SimpleDateFormat FormatoFecha = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        String Fecha = FormatoFecha.format(FechaEnvio.getTime());
+        String Estado = Leido ? "LEIDO" : "SIN LEER";
+        
+        return "DE" + Emisor + "\n" + "ASUNTO:" + Asunto + "\n" + "CONTENIDO: " + Contenido + "\n" + "FECHA: " + Fecha + "\n" + "ESTADO: " + Estado;
+        
+    }
     
 }
